2de2e61c877bc7863c015d2ba88f1cc2a6aec0ee
Dines Nimalthas <dines@joker-it.ch>
2025-04-24 09:18:21 +0200

    Added GH-Pages Deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Portfolio/',   //  ← exakt Repo-Namen einsetzen
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
